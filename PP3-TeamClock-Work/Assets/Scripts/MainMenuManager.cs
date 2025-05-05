7bd90856b45642281685a784f3313c0f16eb5493
Billy-Gatez <JMCAHILL@STUDENT.FULLSAIL.EDU>
2025-05-05 03:09:10 -0700

    PP3 - Prefab project clean

---
+++ PP3-TeamClock-Work/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+// Jeremy Cahill
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+ 
+    public void StartGame()
+    {
+       
+        SceneManager.LoadScene("SampleScene");
+         
+    }
+
+
+    public void MainMenu()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+   
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game.");
+        Application.Quit();
+
+       
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
