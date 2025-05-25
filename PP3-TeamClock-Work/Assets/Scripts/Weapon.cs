d917889d95facd6d213f85d93fe45057256b7f19
George Carter <GTCarter@student.fullsail.edu>
2025-05-25 10:35:08 -0500

    Added code to Shoot Attack Upgrade
    Added code to Shoot Attack Upgrade Recoil Amount, Bullet Speed, Bullet, Drop Rate, and Penetration Power.

--- PP3-TeamClock-Work/Assets/Scripts/Weapon.cs
+++ PP3-TeamClock-Work/Assets/Scripts/Weapon.cs
@@ -2,15 +2,72 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("--- Weapon Properties ---")]
+    public float recoilAmount;
+    public float bulletSpeed;
+    public float bulletDropRate;
+    public float penetrationPower;
+    public AudioClip hitMarkerSound;
+    public GameObject hitEffectPrefab;
+
+    private float recoilRecoveryTime = 0.2f;
+    private Vector3 recoilOffset;
+    private Camera cam;
+    private AudioSource aud;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        aud = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RecoverRecoil();
+    }
+
+    public void FireWeapon()
+    {
+        RaycastHit hit;
+        Vector3 direction = cam.transform.forward + recoilOffset;
+
+        if (Physics.Raycast(cam.transform.position, direction, out hit, bulletSpeed))
+        {
+            HandleHit(hit);
+            ApplyPenetration(hit);
+        }
+
+        ApplyRecoil();
+    }
+
+    void ApplyRecoil()
+    {
+        recoilOffset += new Vector3(Random.Range(-recoilAmount, recoilAmount), Random.Range(-recoilAmount, recoilAmount), 0);
+    }
+
+    void RecoverRecoil()
+    {
+        recoilOffset = Vector3.Lerp(recoilOffset, Vector3.zero, Time.deltaTime / recoilRecoveryTime);
+    }
+
+    void HandleHit(RaycastHit hit)
+    {
+        Instantiate(hitEffectPrefab, hit.point, Quaternion.identity);
+        aud.PlayOneShot(hitMarkerSound);
+
+        IDamage dmg = hit.collider.GetComponent<IDamage>();
+        if (dmg != null)
+        {
+            dmg.takeDamage((int)penetrationPower);
+        }
+    }
+
+    void ApplyPenetration(RaycastHit hit)
+    {
+        if (hit.collider.CompareTag("ThinObject"))
+        {
+            Physics.Raycast(hit.point + hit.normal * 0.1f, cam.transform.forward, out hit, bulletSpeed);
+            HandleHit(hit);
+        }
     }
 }
